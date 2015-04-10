c970c2c937b197d2b2f9b3f60ec55125c7bfba3e
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-04-09 21:21:10 -0400

    fix test cases

--- mora-core/src/test/resources/db/JdbcPasswordDao.sql
+++ mora-core/src/test/resources/db/JdbcPasswordDao.sql
@@ -1,3 +1,4 @@
 insert into t_user(id, username, email, firstname, lastname, deleted, creation_date, last_update) values(1, 'ray.sponsible', 'ray.sponsible@gmail.com', 'Ray', 'Sponsible', 0, '2014-01-01 10:30:55', '2014-12-01 14:30:55');
-
 insert into t_password(id, user_id, value, creation_date, last_update) values(1, 1, 'bc9a50e1e0085b13c4bba866f6dfe57c', '2014-01-01 10:30:55', '2014-12-01 14:30:55');
+
+insert into t_user(id, username, email, firstname, lastname, deleted, creation_date, last_update) values(2, 'john.doe', 'john.doe@gmail.com', 'John', 'Dow', 0, '2014-01-01 10:30:55', '2014-12-01 14:30:55');
