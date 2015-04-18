02b7968b4d93092f99839afbf7b0cd9c44b3f4d9
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-04-18 13:19:47 -0400

    fix sonar issues

--- mora-core/src/main/java/tchepannou/mora/core/config/DaoConfig.java
+++ mora-core/src/main/java/tchepannou/mora/core/config/DaoConfig.java
@@ -3,7 +3,6 @@ package tchepannou.mora.core.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import tchepannou.mora.core.dao.AccessTokenDao;
--- mora-rest-security/src/main/java/tchepannou/mora/rest/security/Application.java
+++ mora-rest-security/src/main/java/tchepannou/mora/rest/security/Application.java
@@ -3,7 +3,6 @@ package tchepannou.mora.rest.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import tchepannou.mora.rest.security.config.Config;
 
