f98568b797513577ec9cd9a2f12eb4ea6d37e99a
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-04-20 09:53:46 -0400

    add post microservice

---
+++ mora-rest-post/src/main/java/tchepannou/mora/rest/post/Application.java
@@ -0,0 +1,17 @@
+package tchepannou.mora.rest.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
+import tchepannou.mora.rest.post.config.Config;
+
+@SpringBootApplication
+@EnableCaching
+@Import ({Config.class})
+public class Application {
+    //-- Main
+    public static void main (String [] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
