1bb29ae16c0c1a92255060346e7a4c054e122e3b
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-04-21 23:15:05 -0400

    add installation script

--- etc/install.sh
+++ etc/install.sh
@@ -3,13 +3,14 @@
 APP=$1
 JAR="$APP-1.0-exec.jar"
 
-mkdir /opt/$APP
-cp ~/repo/$APP/target/$JAR /opt/$APP/$JAR
+if test ! -d "/opt/$APP"; then
+  mkdir /opt/$APP
+fi
+cp ~/repo/mora/$APP/target/$JAR /opt/$APP/$JAR
 
 if test ! -f "/etc/init.d/$APP"; then
-    ln -s /opt/$APP/$JAR /etc/init.d/$APP
+    ln -s ~/repo/mora/$APP/etc/initd.sh /etc/init.d/$APP
     chmod +x /etc/init.d/$APP
-;fi
+fi
 
-/etc/init.d/$APP restart
-
+#/etc/init.d/$APP restart
