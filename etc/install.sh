511e85040cbebd6c92a33220896b6d30df6d1754
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-04-23 07:27:38 -0400

    fix service

--- etc/install.sh
+++ etc/install.sh
@@ -10,11 +10,11 @@ fi
 if test ! -d "/opt/$APP/log"; then
   mkdir /opt/$APP/log
 fi
-cp ../repo/mora/$APP/target/$APP-1.0-exec.jar /opt/$APP/$APP.jar
+cp ../$APP/target/$APP-1.0-exec.jar /opt/$APP/$APP.jar
 chown -R $USER:$GROUP /opt/$APP
 
 if test ! -f "/etc/init.d/$APP"; then
-    cp ../repo/mora/$APP/etc/initd.sh /etc/init.d/$APP
+    cp ../$APP/etc/initd.sh /etc/init.d/$APP
 fi
 chmod +x /etc/init.d/$APP
 
