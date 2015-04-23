193a2d4a0f3675b3899165c1f3069b8f353ef48d
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-04-23 09:02:43 -0400

    startup configuration

--- mora-rest-post/etc/initd.sh
+++ mora-rest-post/etc/initd.sh
@@ -8,13 +8,14 @@
 SERVICE_NAME=mora-rest-post
 PATH_TO_JAR=/opt/mora-rest-post/mora-rest-post.jar
 PID_PATH_NAME=/opt/mora-rest-post/mora-rest-post.pid
+USER_ID=mora
 
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            su - USER_ID -c "java -jar $PATH_TO_JAR > /dev/null 2>&1  &"
+            echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is already running ..."
@@ -31,19 +32,13 @@ case $1 in
             echo "$SERVICE_NAME is not running ..."
         fi
     ;;
+
     restart)
-        if [ -f $PID_PATH_NAME ]; then
-            PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stopping ...";
-            kill $PID;
-            echo "$SERVICE_NAME stopped ...";
-            rm $PID_PATH_NAME
-            echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
-        else
-            echo "$SERVICE_NAME is not running ..."
-        fi
+        $0 stop
+        $0 start
     ;;
+
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
 esac
