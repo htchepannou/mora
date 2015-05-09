6de02fd0f185463b4151618baf83a4d81701a11e
Herve Tchepannou <herve.tchepannou@gmail.com>
2015-05-09 17:43:24 -0400

    simplify class structure

--- mora-rest-security/src/main/java/tchepannou/mora/rest/security/Application.java
+++ mora-rest-security/src/main/java/tchepannou/mora/rest/security/Application.java
@@ -3,13 +3,42 @@ package tchepannou.mora.rest.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import tchepannou.mora.rest.security.config.Config;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import tchepannou.mora.core.config.CacheConfig;
+import tchepannou.mora.core.config.ExtConfig;
+import tchepannou.mora.core.config.ServiceConfig;
+import tchepannou.mora.insidesoccer.config.IsConfig;
+import tchepannou.mora.rest.core.security.AbstractRestSecurityConfig;
 
 @SpringBootApplication
 @EnableCaching
-@Import ({Config.class})
-public class Application {
+@Configuration
+@Import ({ExtConfig.class, IsConfig.class, ServiceConfig.class, CacheConfig.class})
+public class Application extends AbstractRestSecurityConfig {
+    //-- AbstractSecurityConfig overrides
+    @Override
+    protected void configureAuthorization(HttpSecurity http) {
+        try{
+            // @formatter:off
+            http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/roles/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/users/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/users/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/access_token/**").permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                    .anonymous().disable()
+            ;
+            // @formatter:om
+        } catch (Exception e){
+            throw new IllegalStateException("Unable to configure authorizations", e);
+        }
+    }
+
     //-- Main
     public static void main (String [] args){
         SpringApplication.run(Application.class, args);
--- mora-rest-space/src/main/java/tchepannou/mora/rest/space/Application.java
+++ mora-rest-space/src/main/java/tchepannou/mora/rest/space/Application.java
@@ -3,13 +3,43 @@ package tchepannou.mora.rest.space;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import tchepannou.mora.rest.space.config.Config;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import tchepannou.mora.core.config.CacheConfig;
+import tchepannou.mora.core.config.ExtConfig;
+import tchepannou.mora.core.config.ServiceConfig;
+import tchepannou.mora.insidesoccer.config.IsConfig;
+import tchepannou.mora.rest.core.security.AbstractRestSecurityConfig;
 
 @SpringBootApplication
 @EnableCaching
-@Import ({Config.class})
-public class Application {
+@EnableWebMvcSecurity
+@Configuration
+@Import ({ExtConfig.class, IsConfig.class, ServiceConfig.class, CacheConfig.class})
+public class Application  extends AbstractRestSecurityConfig {
+    //-- AbstractSecurityConfig overrides
+    @Override
+    protected void configureAuthorization(HttpSecurity http){
+        try {
+            // @formatter:off
+            http
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/spaces/**").permitAll()
+                    .antMatchers(HttpMethod.GET, "/members/**").permitAll()
+                    .anyRequest()
+                        .authenticated()
+                    .and()
+                        .anonymous().disable()
+            ;
+            // @formatter:on
+        } catch (Exception e){
+            throw new IllegalStateException("Unable to configure authorizations", e);
+        }
+    }
+
     //-- Main
     public static void main (String [] args){
         SpringApplication.run(Application.class, args);
